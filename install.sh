e9f94831ae19bb6a4c0a87ecb3646480f42289f6
vel <powervel1995@gmail.com>
2023-06-23 15:12:35 +0530

    :tada: :sparkles: initial commit

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/tmux/session-manager.sh ~/session-manager.sh
+ln -sf ~/dotfiles/tmux/session-manager.sh ~/.local/bin/t
+ln -sf ~/dotfiles/config/.user_alias ~/.user_alias
+ln -sf ~/dotfiles/config/.user_config ~/.user_config
